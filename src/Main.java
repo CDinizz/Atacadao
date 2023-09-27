3219d35033b7bf2262ba31b8b2e7653c193574ff
CDinizz <139877164+CDinizz@users.noreply.github.com>
2023-09-27 15:37:10 -0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,6 @@
-
-
 public class Main {
-
     public static void main(String[] args) {
-        CaixaDoAtacado.computarCompra();
-
+        CaixaDoAtacado caixa = new CaixaDoAtacado();
+        caixa.computarCompra();
     }
 }
-
-
-
-
-
-
-
-
-
